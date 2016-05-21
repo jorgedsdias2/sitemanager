b42d6bd7a6d3711990bf133fa2276c4a73a930c3
Jorge <jorgedsdias@gmail.com>
2016-05-21 18:59:42 +0000

    faltou a codificacao

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class SessionsController < ApplicationController
 	layout 'login'
 	before_filter :require_user, only: [:index]
--- spec/support/sessions_helper.rb
+++ spec/support/sessions_helper.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 module SessionsHelper
 	def login(user)
 		visit login_path
