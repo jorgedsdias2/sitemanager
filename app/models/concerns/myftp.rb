92f029d63e3256b2c00f9e1ca9c055581feaddac
Jorge <jorgedsdias@gmail.com>
2016-08-13 14:19:51 +0000

    Correcao do nome do concern de my_ftp para myftp pois estava dando erro nos testes com RSpec

--- app/models/concerns/myftp.rb
+++ app/models/concerns/myftp.rb
@@ -15,7 +15,7 @@ module Myftp
 		def ftp_params(name)
 			ftp_params = {
 				:absolute_path => "http://www.{my_site}.com.br",
-				:path => {"/{path}/images/:style/:id/:filename"},
+				:path => "/{path}/images/:style/:id/:filename",
 				:url => "/images/:style/:id/:filename",
 				:host => "{host}",
 				:user => "{user}",
