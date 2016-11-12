9bdd7007b4a29bc26ba507b634c2d63981b829ef
Jorge <jorgedsdias@gmail.com>
2016-11-12 22:45:44 +0000

    Mais uma vez

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,9 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
-	factory :user do |u|
-		u.id 1
-		u.name 'User'
-		u.email 'user@email.com'
-		u.password '123456'
-		u.password_confirmation '123456'
-		u.group_id 1
-		u.role 'user'
-  end
-
-  factory :other_user do |u|
-    u.id 2
-    u.name 'Other User'
-    u.email 'other@email.com'
+  factory :user do |u|
+    u.id 1
+    u.name 'User'
+    u.email 'user@email.com'
     u.password '123456'
     u.password_confirmation '123456'
     u.group_id 1
