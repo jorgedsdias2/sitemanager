3bed7f5f09b05f9cebada3178b34c3ebd70962ba
Jorge <jorgedsdias@gmail.com>
2016-11-12 23:01:22 +0000

    Bug no travis so pode

--- spec/support/users_helper.rb
+++ spec/support/users_helper.rb
@@ -12,7 +12,6 @@ module UsersHelper
 
   private
   def user_form(user)
-    puts user.inspect
     group = FactoryGirl.build(:group, name: 'Administrator')
     fill_in 'user[name]', with: user.name
     fill_in 'user[email]', with: user.email
