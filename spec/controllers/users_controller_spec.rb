f13ed7a764b4ac7f0fb3ab0adfde88650175c111
Jorge <jorgedsdias@gmail.com>
2016-11-12 17:14:52 +0000

    Subindo teste de indentacao

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,55 +2,55 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-	render_views
-	subject{ page }
-	let(:group) { FactoryGirl.create(:group) }
-	let(:user) { FactoryGirl.create(:user, group_id: group.id) }
+  render_views
+  subject { page }
+  let(:group) { FactoryGirl.create(:group) }
+  let(:user) { FactoryGirl.create(:user, group_id: group.id) }
 
-	# GET index
-	describe "GET index" do
-		describe "list all users" do
-			before do
-				logged_as(user)
-				visit users_path
-			end
+  # GET index
+  describe "GET index" do
+    describe "list all users" do
+      before do
+        logged_as(user)
+        visit users_path
+      end
 
-			it { should have_selector('.table tbody>tr', :minimum => 1) }
-			#it { puts all(".table tbody>tr").count }
-			#it { expect(assigns('users')).to eq([user]) }
-		end
-	end
+      it { should have_selector('.table tbody>tr', :minimum => 1) }
+      #it { puts all(".table tbody>tr").count }
+      #it { expect(assigns('users')).to eq([user]) }
+    end
+  end
 
-	# GET new
-	describe "GET new" do
-		describe "new user" do
-			before do
-				logged_as(user)
-				visit new_user_path
-			end
+  # GET new
+  describe "GET new" do
+    describe "new user" do
+      before do
+        logged_as(user)
+        visit new_user_path
+      end
 
-			it { should have_text(t('text.user.register')) }
-		end
+      it { should have_text(t('text.user.register')) }
+    end
   end
 
   # GET edit
   describe "GET edit" do
     describe "edit user" do
-			before do
-				logged_as(user)
+      before do
+        logged_as(user)
         visit edit_user_path(user)
       end
 
       it { should have_text(t('text.user.update')) }
-		end
+    end
   end
 
-	describe "renders the panel template" do
-		before do
-			logged_as(user)
-			visit users_path
-		end
+  describe "renders the panel template" do
+    before do
+      logged_as(user)
+      visit users_path
+    end
 
-		it { should render_template("panel") }
-	end
+    it { should render_template("panel") }
+  end
 end
