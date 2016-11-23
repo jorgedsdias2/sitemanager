dd7fd72128b6d6455255955bbd475945b944e99b
Jorge <jorgedsdias@gmail.com>
2016-11-23 00:54:37 +0000

    Subindo arquivo de upload controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # -*- encoding : utf-8 -*-
 Rails.application.routes.draw do
-	resources :users
+  resources :users
   resources :groups
-	resources :pages
-	resources :sessions
-	
-	get 'index', to: 'sessions#index'
-	get 'panel', to: 'sessions#new'
-	get 'login', to: 'sessions#new'
-	post 'login', to: 'sessions#create'
-	get 'logout', to: 'sessions#destroy'
-	get 'dashboard', to: 'sessions#index'	
+  resources :pages
+  resources :sessions
+  resources :uploads
+  post 'uploads_destroy', to: 'uploads#destroy'
+
+  get 'index', to: 'sessions#index'
+  get 'panel', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  get 'dashboard', to: 'sessions#index'
 end
