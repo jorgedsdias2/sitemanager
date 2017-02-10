4b1fe798216bebe04bcec9195d6b49dc2b5cd504
jorge <jorgedsdias@gmail.com>
2017-02-10 22:16:41 +0000

    Deletando

--- db/migrate/20161114170145_create_pages_and_uploads.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-class CreatePagesAndUploads < ActiveRecord::Migration
-  def change
-    create_table :pages_uploads, id: false do |t|
-      t.belongs_to :page, index: true, foreign_key: true
-      t.belongs_to :upload, index: true, foreign_key: true
-    end
-  end
-end
