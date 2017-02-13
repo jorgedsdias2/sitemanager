9eca7237d6e1464eecc1444f51161ee84f1a03f1
jorge <jorgedsdias@gmail.com>
2017-02-13 01:17:52 +0000

    Alteracoes factorie page

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,14 +1,14 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
-	factory :page do |p|
-		p.title 'Page'
-		p.summary 'Page'
-		p.description 'One Page'
-		p.nickname 'page'
-		p.link 'http://www.page.com.br'
-		p.target '_self'
-		p.order 1
-		p.status 'A'
-		p.user_id 1
+	factory :page do
+		title 'MyString'
+		summary 'MyString'
+		description 'MyText'
+		nickname 'MyString'
+		link 'MyString'
+		target 'MyString'
+		order 1
+		status 'A'
+		user nil
 	end
 end
